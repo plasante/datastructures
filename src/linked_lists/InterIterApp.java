72fd8fc4826a3ffaa004b5475a8792f57079c18f
Pierre Lasante <pierre.lasante@videotron.ca>
2013-11-14 21:53:49 -0500

    Reached chapter 6 recursion

--- src/linked_lists/InterIterApp.java
+++ src/linked_lists/InterIterApp.java
@@ -3,7 +3,7 @@ package linked_lists;
 import java.io.*;
 
 public class InterIterApp {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		LinkList9 theList = new LinkList9();
 		ListIterator iter1 = theList.getIterator();
 		long value;
@@ -22,12 +22,45 @@ public class InterIterApp {
 			
 			switch(choice) {
 				case 's':
+					if( !theList.isEmpty())
+						theList.displayList();
+					else
+						System.out.println("List is empty");
+					break;
 				case 'r':
+					iter1.reset();
+					break;
 				case 'n':
+					if( !theList.isEmpty() && !iter1.atEnd() )
+						iter1.nextLink();
+					else
+						System.out.println("Can't go to next link");
+					break;
 				case 'g':
+					if( !theList.isEmpty() ) {
+						value= iter1.getCurrent().dData;
+						System.out.println("Returned " + value);
+					} else
+						System.out.println("List is empty");
+					break;
 				case 'b':
+					System.out.println("Enter value to insert: ");
+					System.out.flush();
+					value = getInt();
+					iter1.insertBefore(value);
+					break;
 				case 'a':
+					System.out.println("Entere value to insert: ");
+					System.out.flush();
+					value = getInt();
+					iter1.insertAfter(value);
+					break;
 				case 'd':
+					if( !theList.isEmpty() ) {
+						value = iter1.deleteCurrent();
+						System.out.println("Deleted " + value);
+					} else
+						System.out.println("Can't delete");
 					break;
 				default:
 					System.out.println("Invalid entry");
@@ -35,16 +68,21 @@ public class InterIterApp {
 		}
 	} // end main
 	
-	public static String getString() {
-		return "";
+	public static String getString() throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
 	}
 	
-	public static char getChar() {
-		return 'a';
+	public static char getChar() throws IOException {
+		String s = getString();
+		return s.charAt(0);
 	}
 	
-	public static int getInt() {
-		return 0;
+	public static int getInt() throws IOException {
+		String s = getString();
+		return Integer.parseInt(s);
 	}
 }
 
